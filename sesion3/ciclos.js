9ca301fdaa6ec95d5ff20720b021d5f2f7b288fd
Andres Felipe Osorio Marin <andres.osorio@arus.com.co>
2022-03-24 08:57:00 -0500

    ciclos sesion3

---
+++ sesion3/ciclos.js
@@ -0,0 +1,85 @@
+
+let nombre = ['juan','pedro','mario'] ; 
+let materia = new Array('sociales','mate','ingles') ;
+let ciudades = "medellín, bogota, cali".split(','); 
+
+
+//const estudiantes = [
+//{nombre: "rosmira", carrera: "ingles", semestre: 2},
+//{nombre: "pacho", carrera: "mates", semestre: 2},
+//{nombre: "rosmira", carrera: "español", semestre: 2},
+//];
+
+
+
+/*const estudiantes = [
+
+  { nombre: "Rosmira", carrera: "Matematico", semestre: 2, sexo: "F" },
+
+  { nombre: "Patricia", carrera: "Matematico", semestre: 13, sexo: "F" },
+
+  { nombre: "Guille", carrera: "Derecho", semestre: 5, sexo: "M" },
+
+  { nombre: "Leon", carrera: "Artes", semestre: 7, sexo: "M" },
+
+];
+
+*/
+
+//for (const estudiante of estudiantes) {
+//console.log(estudiante.nombre)
+//}
+
+
+//const respuesta = estudiantes.filter ((c) => c.semestre >=5) 
+
+//console.log(respueta);
+
+
+
+
+
+
+
+
+
+
+
+//for
+//while
+//do while 
+//for each 
+
+//for (let i = 0; i < nombres.lenght; i ++) { 
+
+//    console.log(nombres[i]);
+
+
+//}
+
+
+//for (const key in materia){ 
+  //  console.log(materia[key]);
+//}
+
+
+//console.log(ciudades); 
+
+//for (const ciudades of ciudad) {
+//console.log(ciudades)
+//}
+
+//ciudades.forEach((ciudad) => {
+    //console.log(ciudad);
+    //if (ciudad === "medellín") { 
+        //break;
+  //  }
+//}   
+
+
+console.log("hola")
+let n = 0;
+while (n < 3 ) {
+  console.log(n);
+  n = 200
+} 
