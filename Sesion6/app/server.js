71dcb62cd4b191c2b552eb79f27872fea5b1ced7
Andres Felipe Osorio Marin <andres.osorio@arus.com.co>
2022-04-05 09:00:01 -0500

    sequalize y servers

---
+++ Sesion6/app/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors= require("cors");
+const app = express();
+
+let corsOptions = {
+origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({extends: true }));
+
+app.get("/", (req, res) => { 
+res.json({mensaje: "Bienvenidos a mi primer api consss express" });
+}); 
+
+const PORT= 8081;
+
+app.listen(PORT, () => { 
+    console.log(`server corriendo por el puerto: ${PORT}`);
+}); 
